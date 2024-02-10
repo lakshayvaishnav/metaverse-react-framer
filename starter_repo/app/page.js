67b225f291f211d6d4bb535cc2e0c7b272f35266
lakshayvaishnav <123557766+lakshayvaishnav@users.noreply.github.com>
2024-02-10 20:21:37 +0530

    completed 3 more pages including footer and feedback

--- starter_repo/app/page.js
+++ starter_repo/app/page.js
@@ -19,14 +19,17 @@ const Page = () => (
       <div className="gradient-03 z-0" />
       <Explore />
     </div>
-    <div classname="relative">
+    <div className="relative">
       <GetStarted />
       <div className="gradient-04 z-0" />
-    <WhatsNew />
+      <WhatsNew />
     </div>
     <World />
-    <Insights />
-    <Feedback />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
     <Footer />
   </div>
 );
